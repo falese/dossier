1ee533f3a7d59247635866b5faa53ab4dc15ae3d
falese <falese@gmail.com>
2016-11-05 21:45:01 -0700

    addition of annotate, annotated user model

--- dossier/app/models/user.rb
+++ dossier/app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+
 class User < ApplicationRecord
 end
--- dossier/test/models/user_test.rb
+++ dossier/test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
